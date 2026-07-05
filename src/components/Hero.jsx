f4aa48d7fb1690c1f140baf7114da1fe31e3d030
khushbusaifi012 <khushbusaifi27@gmail.com>
2026-07-05 21:09:51 +0530

    Updated resume drive link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -112,7 +112,7 @@ const Hero = () => {
           {/* CTA Buttons */}
           <motion.div variants={item} className="flex justify-center md:justify-start mt-4 w-full">
             <motion.a
-              href="https://drive.google.com/file/d/1Tmv93pM4onbH7Dvojqd7D7pW8j0NbU1B/view?usp=sharing"
+              href="https://drive.google.com/file/d/1Rd_Xl1DNVBccrb7nOvjUyFXMHZuO9yGb/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               whileHover={{ scale: 1.05 }}
